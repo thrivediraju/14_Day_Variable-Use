b880a080b34afc9184772fd4111ab7e5ec707dd1
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-13 17:01:27 +0530

    Add files via upload

---
+++ variable_use.py
@@ -0,0 +1,29 @@
+# a=6
+# print(a); print(a)  
+# a=9
+# print(a,a)  
+
+
+'''1st Way of assigning the values and printing them'''
+# p=2 
+# q=3
+# r=4
+# s=5
+# print(p)
+# print(q)
+# print(r)
+# print(s)
+# print(p,q,r,s)
+
+'''2nd Way of assigning the values and printing them'''
+# p=2; q=3;  r=4; s=5
+# print(p); print(q); print(r); print(s)
+# print(p,q,r,s)
+
+'''3rd  Way of assigning the values and printing them'''
+# p=r=4; q=2; s=5
+# print(p); print(q); print(r); print(s)
+
+'''4th Way of assigning the values and printing them'''
+# p,q,r,s=1,2,3,4
+# print(p); print(q); print(r); print(s)
